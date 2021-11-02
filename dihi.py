19b00ae8be92a69a6818ab4faeb7e78b6716b3c8
Bang-Jack123 <93056023+Bang-Jack123@users.noreply.github.com>
2021-11-02 18:05:52 +0700

    Update dihi.py

--- dihi.py
+++ dihi.py
@@ -67,8 +67,8 @@ IP = requests.get('https://api.ipify.org').text
 def banner():
 	print (''' %s 
  © Group%s
-
- ____ __ __ ____ _____
+kami
+ ____ __ __ ____ _____ 
  | _ \| \/ | __ )| ___| %s> %s DIHI
  | | | | |\/| | _ \| |_  %s> %sMULTI
  | |_| | | | | |_) | _| %s>%sBROT
@@ -77,7 +77,7 @@ def banner():
  %s[%s*%s] --------------------------------------
  [%s*%s] IP : %s%s
 ''' % ( H , K , H , K , H , K , H , K , H , K , P , K , P , H , P , K , P , K , P , H , IP ))
-kami
+
 # MASUK TOKEN (TOKEN LISTRIK)
 header = {"x-fb-connection-bandwidth": str(random.randint(20000000.0, 30000000.0)), "x-fb-sim-hni": str(random.randint(20000, 40000)), "x-fb-net-hni": str(random.randint(20000, 40000)), "x-fb-connection-quality": "EXCELLENT", "x-fb-connection-type": "cell.CTRadioAccessTechnologyHSDPA", "user-agent": "NokiaC3-00/5.0 (07.20) Profile/MIDP-2.1 Configuration/CLDC-1.1 Mozilla/5.0 AppleWebKit/420+ (KHTML, like Gecko) Safari/420+ ;]", "content-type": "application/x-www-form-urlencoded", "x-fb-http-engine": "Liger"}
 def masuk():
